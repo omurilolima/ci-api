02790af11f38ca9e564a9a541be18ad5a5ed274f
Murilo Lima <msl@cesar.school>
2023-02-21 18:38:33 +0000

    Display any API exceptions in a modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,6 +46,7 @@ async function postForm(e) {
     if (response.ok) {
         displayErrors(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 
@@ -85,7 +86,10 @@ async function getStatus(e) {
 
     if (response.ok) {
         displayStatus(data);
-    } throw new Error(data.error);
+    } else {
+        displayException(data);
+        throw new Error(data.error);
+    };
 }
 
 function displayStatus(data) {
@@ -95,6 +99,21 @@ function displayStatus(data) {
     let heading = 'API Key Status';
     let results = `<div>Your key is valid until</div>`;
     results += `<div class='key-status'>${data.expiry}</div>`
+
+    document.getElementById('resultsModalTitle').innerText = heading;
+    document.getElementById('results-content').innerHTML = results;
+
+    resultsModal.show();
+
+}
+
+function displayException(data) {
+
+    let heading = `An Exception Occured`;
+
+    results = `<div>The API returnd status code ${data.status_code}</div>`;
+    results += `<div>Error number: <strong>${data.error_no}</strong></div>`;
+    results += `<div>Error text: <strong>${data.error}</strong></div>`;
 
     document.getElementById('resultsModalTitle').innerText = heading;
     document.getElementById('results-content').innerHTML = results;
